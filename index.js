90f9dd1bba1cf1ba9339613b4e43f1b5ce208a54
Omar Ch <mhcomar28@gmail.com>
2018-01-12 07:51:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  return ("I can't hear you!".toUpperCase())
+  return string.toLowerCase()
 }
 
 function sayHiToGrandma(string)
